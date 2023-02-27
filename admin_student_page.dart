f5c73fbb341523bae3ec2162c5d9ebed309af2ce
Nasibali <72644180+Nasibali144@users.noreply.github.com>
2023-02-27 11:05:26 +0500

    Add files via upload

---
+++ admin_student_page.dart
@@ -0,0 +1,38 @@
+import 'package:e_learning/databases/students.dart';
+import 'package:e_learning/pages/admin_pages/admin_dashboard_page.dart';
+import 'package:e_learning/pages/admin_pages/admin_students.dart';
+import 'package:e_learning/pages/admin_pages/groups/admin_students_edit.dart';
+import 'package:e_learning/pages/base_page.dart';
+import '../../services/io_service.dart';
+
+class AdminStudentsPage extends BaseScreen {
+
+  @override
+  void build() {
+    super.build();
+    for(int i = 0; i < students.length; i++){
+      print('${i+1}. ${students[i].firstname} ${students[i].lastname}');
+    }
+    print('${students.length+1}. Back');
+    int select = io.number;
+    if(select>0 && select<students.length){
+      studentsInfo(select);
+      print('1. Edit');
+      print('2. Delete');
+      print('3. Back');
+      int command = io.number;
+
+      if(command == 1){
+        studentsEdit(select-1);
+      }else if(command == 2){
+        students.removeAt(select-1);
+        AdminStudentsPage();
+      }else{
+        AdminStudentsPage();
+      }
+
+    }else{
+      AdminDashboardPage();
+    }
+  }
+}
